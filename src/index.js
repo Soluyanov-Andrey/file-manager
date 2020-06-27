dbbf17d8bf1ea99b6042cfa4801284233e25e296
a.soluyanov <a--b--s@yandex.ru>
2020-06-28 00:30:23 +0400

    Первые компаненты

--- src/index.js
+++ src/index.js
@@ -3,7 +3,40 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+
+const Home=()=>{
+    return(
+        <div id="Home">My Todo list</div>
+    );
+};
+
+const Data=()=>{
+    return(
+        <div id="Type">My Todo list</div>
+    );
+};
+
+const Type=()=>{
+    return(
+        <div id="Type">My Todo list</div>
+    );
+};
+
+const Size=()=>{
+    return(
+        <div id="Size">My Todo list</div>
+    );
+};
+const el = (
+    <div>
+        <Home/>
+        <Data/>
+        <Type/>
+        <Size/>
+    </div>
+)
+
 ReactDOM.render(
-  <App />,
+    el,
   document.getElementById('root')
 );
