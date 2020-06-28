b2cc37b33a6ddee89cf2365136ea382ae99f2443
a.soluyanov <a--b--s@yandex.ru>
2020-06-28 22:01:48 +0400

    Переделали Cap под компанент класса

--- src/components/TableFiles/Сap/Cap.js
+++ src/components/TableFiles/Сap/Cap.js
@@ -1,22 +1,22 @@
-import React from "react";
+import React, {Component} from 'react';
 import NameCap from "./Name/NameCap";
 import DataCap from "./Data/DataCap"
 import TypeCap from "./Type/TypeCap";
 import SizeCap from "./Size/SizeCap";
 
-const Cap = () => {
-    return (
-        <div id="Cap">
-            <NameCap/>
-               <div id="NameCapPartition"/>
-            <DataCap/>
-               <div id="DataCapPartition"/>
-            <TypeCap/>
-               <div id="TypeCapPartition"/>
-            <SizeCap/>
-               <div id="SizeCapPartition"/>
-        </div>
-    );
+export default class Cap extends Component {
+    render() {
+        return (
+            <div id="Cap">
+                <NameCap/>
+                  <div id="NameCapPartition"/>
+                <DataCap/>
+                  <div id="DataCapPartition"/>
+                <TypeCap/>
+                  <div id="TypeCapPartition"/>
+                <SizeCap/>
+                  <div id="SizeCapPartition"/>
+            </div>
+        );
+    }
 };
-
-export default Cap;
