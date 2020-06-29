16bc80cb34b9a2f90fcdb173216bd01036e80cfb
a.soluyanov <a--b--s@yandex.ru>
2020-06-29 18:43:00 +0400

    Выбираем все class

--- src/components/TableFiles/Сap/Cap.js
+++ src/components/TableFiles/Сap/Cap.js
@@ -3,13 +3,22 @@ import NameCap from "./Name/NameCap";
 import DataCap from "./Data/DataCap"
 import TypeCap from "./Type/TypeCap";
 import SizeCap from "./Size/SizeCap";
-
+//Код демонстрирует как можно выбрать все class в коде и их изменить
 export default class Cap extends Component {
+    onLabelClick = () => {
+        const demoClasses = document.querySelectorAll('.NameBlok');
+
+        demoClasses.forEach(element => {
+            element.style.width = "150px";
+        });
+        // document.getElementById('NameBlok').style.width = "150px"
+
+    };
     render() {
         return (
             <div id="Cap">
                 <NameCap/>
-                  <div id="NameCapPartition"/>
+                  <div onClick={this.onLabelClick} id="NameCapPartition"/>
                 <DataCap/>
                   <div id="DataCapPartition"/>
                 <TypeCap/>
