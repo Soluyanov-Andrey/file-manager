a95787f2daa2289b580dfb482c7550f7494e1183
a.soluyanov <a--b--s@yandex.ru>
2020-07-14 23:07:29 +0400

    API redux-hooks

--- src/Table/Ramka/Ramka.js
+++ src/Table/Ramka/Ramka.js
@@ -1,27 +1,16 @@
 import React from "react";
 import "./Ramka.css"
-import {connect} from 'react-redux'
+import { useSelector } from 'react-redux'
 
-const Ramka = ({counter}) => {
-    console.log(counter.line[0].name)
+const Ramka = () => {
+    const counter = useSelector(state => state)
+    console.log({counter})
     return (
         <div className="Ramka" >
-            <h4>{counter.line[0].name}</h4>
+            <h4>44</h4>
         </div>
     );
 };
-function mapStateToProps(state) {
 
-    return{
-        counter: state
-    }
-}
 
-function mapDispatchToProps(dispatch){
-
-    return{
-
-    }
-}
-
-export default connect(mapStateToProps,mapDispatchToProps)(Ramka)
+export default Ramka
