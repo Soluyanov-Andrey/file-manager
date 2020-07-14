17de77c9a8d0f54022eaafccffb3361b1bd0616b
a.soluyanov <a--b--s@yandex.ru>
2020-07-14 17:45:19 +0400

    добавили redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import rootReducer from './redux/rootreducer';
 
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(
+)));
+const app = (
+    //передаем с помощью провайдера store
+    <Provider store={store}>
+        <App/>
+    </Provider>
+)
 ReactDOM.render(
-  <App />,
+    app,
   document.getElementById('root')
 );
--- src/redux/rootreducer.js
+++ src/redux/rootreducer.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
+const initialState={
 
-import './App.css';
-import Table from "./Table/Table";
-class App extends Component {
-    state = {
         line: [
             { name:'Документ', data: '17.05.2002', type:'Пустая папка',size: 15},
             { name:'Фаил', data: '17.05.2003', type:'Пустая папка',size: 15},
             { name:'текст', data: '17.05.2004', type:'Пустая папка',size: 15}
         ]
-        // pageTitle: 'React components'
+
+
+}
+export default function  rootReducer(state = initialState,action){
+    switch (action.type) {
+
+        case 'ADD':
+            return {
+
+            }
+        case 'SUB':
+            return {
+
+            }
+
     }
 
-  render() {
 
-    return (
-        <Table props={this.state}/>
-    );
-  }
+    return state
 }
-
-export default App;
