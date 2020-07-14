ef37226b2c68663c1fddd4df8c6d8ea26ad5627e
a.soluyanov <a--b--s@yandex.ru>
2020-07-14 22:58:29 +0400

    Вывели данные из вложенногокомпанента

--- src/Table/Ramka/Ramka.js
+++ src/Table/Ramka/Ramka.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./Ramka.css"
-const Ramka = (props) => {
+import {connect} from 'react-redux'
 
+const Ramka = ({counter}) => {
+    console.log(counter.line[0].name)
     return (
         <div className="Ramka" >
-             hhhhhhhhhhhh
+            <h4>{counter.line[0].name}</h4>
         </div>
     );
 };
+function mapStateToProps(state) {
 
-export default Ramka;
+    return{
+        counter: state
+    }
+}
+
+function mapDispatchToProps(dispatch){
+
+    return{
+
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Ramka)
