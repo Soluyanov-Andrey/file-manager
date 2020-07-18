0cc8b62dea6e452336537fc830fa7e0408fb39e7
a.soluyanov <a--b--s@yandex.ru>
2020-07-18 13:02:56 +0400

    вывели в цикли компанент

--- src/App.js
+++ src/App.js
@@ -4,17 +4,37 @@ import Table from "./Table/Table";
 class App extends Component {
     state = {
         line: [
-            { name:'Документ', data: '17.05.2002', type:'Пустая папка',size: 15},
-            { name:'Фаил', data: '17.05.2003', type:'Пустая папка',size: 15},
-            { name:'текст', data: '17.05.2004', type:'Пустая папка',size: 15}
+            { ico:'имя1', name: 'переговоры', type:'Пустая папка',adres_predka:"0",adres_svoi: "0-1"},
+            { ico:'имя2', name: 'переговоры', type:'Пустая папка',adres_predka:"0",adres_svoi: "0-2"},
+            { ico:'имя3', name: 'переговоры', type:'Пустая папка',adres_predka:"0",adres_svoi: "0-3"},
+            { ico:'имя4', name: 'переговоры', type:'Пустая папка',adres_predka:"0-2",adres_svoi: "0-2-1"},
+            { ico:'имя5', name: 'переговоры', type:'Пустая папка',adres_predka:"0-2",adres_svoi: "0-2-2"},
+            { ico:'имя6', name: 'переговоры', type:'Пустая папка',adres_predka:"0-2",adres_svoi: "0-2-3"},
+            { ico:'имя7', name: 'переговоры', type:'Пустая папка',adres_predka:"0-2",adres_svoi: "0-2-4"},
+            { ico:'имя8', name: 'переговоры', type:'Пустая папка',adres_predka:"0-2-3",adres_svoi: "0-2-3-1"},
+            { ico:'имя9', name: 'переговоры', type:'Пустая папка',adres_predka:"0-2-3",adres_svoi: "0-2-3-2"},
+            { ico:'имя10', name: 'переговоры', type:'Пустая папка',adres_predka:"0-3",adres_svoi: "0-3-1"},
+            { ico:'имя11', name: 'переговоры', type:'Пустая папка',adres_predka:"0-3",adres_svoi: "0-3-1"},
+            { ico:'имя12', name: 'переговоры', type:'Пустая папка',adres_predka:"0",adres_svoi: "0-4"},
+            { ico:'имя11', name: 'переговоры', type:'Пустая папка',adres_predka:"0-2-1",adres_svoi: "0-2-1-1"},
+            { ico:'имя12', name: 'переговоры', type:'Пустая папка',adres_predka:"0-2-1",adres_svoi: "0-2-1-2"}
         ]
-        // pageTitle: 'React components'
+
     }
 
   render() {
+      function pow(masiv) {
+          let children = [];
+          let index;
+          for (index = 0; index < masiv; ++index) {
+              children.push(<Table/>)
+          }
+          return children
+      }
+      let showHTMLData = pow(5);
 
     return (
-        <Table props={this.state}/>
+        <div>{showHTMLData}</div>
     );
   }
 }
