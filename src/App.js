cff350dbd0f3c8dd82c3445adde116417efbcc42
a.soluyanov <a--b--s@yandex.ru>
2020-07-24 15:10:06 +0400

    Пример рабочей рекурсии

--- src/App.js
+++ src/App.js
@@ -1,59 +1,78 @@
 import React, { Component } from 'react';
 import './App.css';
 import Table from "./Table/Table";
-class App extends Component {
+const Comments = ({ items }) => (
+    <React.Fragment>
+        {items.map(n => (
+            <div className="comment" key={n.id}>
+                <h3>{n.name}</h3>
+                <div>{n.body}</div>
+                {n.reply && n.reply.length ? <Comments items={n.reply} /> : null}
+            </div>
+        ))}
+    </React.Fragment>
+);
 
-
-    render() {
-        let data = getData();
-
-        function handleData(obj) {
-
-            // возвращаем полученный массив компонентов
-            return [...obj.map((i, n) => <Table data={i} key={n} />)];
-        }
-
-        let showHTMLData = handleData(data);
-
-        return <div className="dropdown hide">{showHTMLData}</div>;
+class App extends React.Component {
+    state = {
+        "comments": [
+            {
+                "name": "Первый комментарий",
+                "body": "Body первого комментария",
+                "date": "2019-05-15T15:56:15.694116Z",
+                "id": "0179ef41-fdb6-4700-a4dc-6d7bbc54385a",
+                "parent": null,
+                "reply": []
+            },
+            {
+                "name": "Второй комментарий",
+                "body": "Body второго комментария",
+                "date": "2019-05-17T13:59:51.167188Z",
+                "id": "1ef06878-58b5-48b0-9349-73986ab66bb4",
+                "parent": null,
+                "reply": []
+            },
+            {
+                "name": "Третий комментарий",
+                "body": "Body третьего комментария",
+                "date": "2019-05-19T12:07:15.613266Z",
+                "id": "5a01231d-3ee9-4bf6-9a50-462a8277898a",
+                "parent": null,
+                "reply": [
+                    {
+                        "name": "Первый вложенный комментарий",
+                        "body": "Body первого вложенного комментария",
+                        "date": "2019-05-21T22:32:44.998207Z",
+                        "id": "514aa634-08bd-4ca3-8a1a-eb10846808ed",
+                        "parent": "5a01231d-3ee9-4bf6-9a50-462a8277898a",
+                        "reply": [
+                            {
+                                "name": "Ещё вложенный комментарий",
+                                "body": "hello, world",
+                                "id": "2957893457984",
+                                "reply": [
+                                    {
+                                        "name": "И ещё",
+                                        "body": "@*(^$#^&#^$)",
+                                        "id": "b56hethsthsw54w",
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
     }
-
+    render() {
+        return (
+            <div>
+                <Comments items={this.state.comments} />
+            </div>
+        );
+    }
 }
 
 export default App;
 
-function getData() {
-    return [
-        {
-            id: 3,
-            parentId: null,
-            name: "name3",
-            children: [
-                {
-                    id: 3.1,
-                    parentId: 3,
-                    name: "name3.1"
-                }
-            ]
-        },
 
-        {
-            id: 2,
-            parentId: null,
-            name: "name2"
-        },
-
-        {
-            id: 1,
-            parentId: null,
-            name: "name1",
-            children: [
-                {
-                    id: 1.1,
-                    parentId: 1,
-                    name: "name1.1"
-                }
-            ]
-        }
-    ];
-}
